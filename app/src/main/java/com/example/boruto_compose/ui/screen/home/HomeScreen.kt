5395dd13e2ec737df1bba3d09eb6c94506787328
JordanMarcelino <jordangamers25@gmail.com>
2022-04-20 22:54:49 +0700

    Testing Remote Mediator and Paging Source

--- app/src/main/java/com/example/boruto_compose/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/boruto_compose/ui/screen/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.boruto_compose.ui.screen.home
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -8,6 +9,7 @@ import androidx.navigation.NavController
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.boruto_compose.ui.common.NinjaList
 import com.example.boruto_compose.ui.navigation.Screen
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @ExperimentalMaterial3Api
 @Composable
@@ -17,6 +19,10 @@ fun HomeScreen(
 ) {
 
     val ninjas = homeViewModel.ninjas.value.collectAsLazyPagingItems()
+    val systemUi = rememberSystemUiController()
+    systemUi.setStatusBarColor(
+        color = MaterialTheme.colorScheme.primary
+    )
 
     Scaffold(
         topBar = {
